1bcf03f2e4ae3641cd897f0593a473492c6c5426
Hubert <marshal604@gmail.com>
2025-03-05 01:28:50 +0800

    feat(services): add EventService for generating events via ApiService

---
+++ src/services/EventService/index.ts
@@ -0,0 +1,13 @@
+import { ApiService } from '@services/ApiService';
+
+export class EventService {
+    private apiService: ApiService;
+
+    constructor(apiService: ApiService) {
+        this.apiService = apiService;
+    }
+
+    async generateEvent(attributes: any, choice: string) {
+        return this.apiService.fetchEvent(attributes, choice);
+    }
+}
